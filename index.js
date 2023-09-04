2c0a7f31627122c1c03b2ab9247f8203360961e6
Maria Herne <noelia.herne@gmail.com>
2023-09-04 11:57:13 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const wakeUpHoursDiv = document.getElementById("wakeup-hours-div")
-const calcBtn = document.getElementById(calcBtn);
-calcBtn.onclick = calcWakeUpTimes;
+const clcBtn = document.getElementById("calc-btn");
+const refreshBtn = document.getElementById("refresh-btn");
+const returnBtn = document.getElementById("return-btn");
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+const promptSection = document.getElementById("prompt-section");
+const imageContainer = document.getElementById("img-container");
+const resultSection = document.getElementById("result-section");
+
+clcBtn.addEventListener("click", calcWakeUpTimes);
+refreshBtn.addEventListener("click", calcWakeUpTimes);
+returnBtn.addEventListener("click", () => {
+  promptSection.classList.remove("hidden");
+  imageContainer.classList.remove("hidden");
+  resultSection.classList.add("hidden");
+});
 
 function calcWakeUpTimes() {
-    const fallAsleepTime = new Date();
-    fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
+  const fallAsleepTime = new Date();
+  fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
 
-    calcBtn.addEventListener("click, calcWakeupTimes")
-    //Function without a name
-    returnbtn.addEventListener(click,() => {
-        promptSection.classList.remove("hidden");
+  const wakeUpTime = new Date(fallAsleepTime);
+  wakeUpHoursDiv.innerHTML = "";
+  for (let i = 1; i <= 6; i++) {
+    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+    const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
+      timeStyle: "short",
     });
-
-    const wakeUpTime = new Date(fallAsleepTime);
-    wakeUpHoursDiv.innerHTML = ""
-
-    for(let i = 1; i <= 6; i++){
-        wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
-        const wakeUpTimeString = (wakeUpTime.toLocaleTimeString("en-US", {timeStyle: "short"}));
-        //console.log(`The first sleep cycle #${i} ends at ${wakeUpTimeString}`);
-        wakeUpTimes.push(wakeUpTimeString);
-    }
     const cycleDiv = document.createElement("div");
-    cycleDiv.classList.add("cycle")
-    cycleDiv.setAttribute("id", `cycle-${i}`)
-    cycleDiv.innerText = wakeUpTimeString
-    wakeUpHoursDiv.appendChild(cycleDiv)
-}
+    cycleDiv.classList.add("cycle");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = wakeUpTimeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
+  }
 
-console.log("Wake-up times:", wakeUpTimes.join(", "));
+  promptSection.classList.add("hidden");
+  imageContainer.classList.add("hidden");
+  resultSection.classList.remove("hidden");
+}
